f40edfd7c35ec0c8d6c847dd0667f3de251c278d
johnny990628 <johnny99457@gmail.com>
2022-04-09 15:28:35 +0800

    feat: patient update and delete

--- models/patient.js
+++ models/patient.js
@@ -1,10 +1,41 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const patientSchema = new mongoose.Schema(
+const cancerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    value: Schema.Types.Mixed,
+})
+
+const reportSchema = new Schema(
     {
         id: {
             type: String,
             required: true,
+        },
+        data: {
+            liver: [cancerSchema],
+            gallbladder: [cancerSchema],
+            kidney: [cancerSchema],
+            pancreas: [cancerSchema],
+            spleen: [cancerSchema],
+            suggestion: [cancerSchema],
+        },
+    },
+    { timestamps: true }
+)
+const patientSchema = new Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+            unique: true,
         },
         blood: {
             type: String,
@@ -29,7 +60,7 @@ const patientSchema = new mongoose.Schema(
         department: String,
         address: String,
         processing: Boolean,
-        reports: [],
+        reports: [reportSchema],
     },
     { timestamps: true }
 )
--- routes/apis/patient.js
+++ routes/apis/patient.js
@@ -3,27 +3,45 @@ const router = express.Router()
 
 const Patient = require('../../models/patient')
 
-router.route('/').post(async (req, res) => {
-    try {
-        const { id, blood, name, gender, birth, phone, department, address, processing, reports = [] } = req.body
-        let patient = new Patient({
-            id,
-            blood,
-            name,
-            gender,
-            birth,
-            phone,
-            department,
-            address,
-            processing,
-            reports,
-        })
-        patient = await patient.save()
-        res.status(200).json(patient)
-    } catch (e) {
-        console.log(e)
-        return res.status(500).json({ message: e.message })
-    }
-})
+router
+    .route('/')
+    .get(async (req, res) => {
+        try {
+            const patients = await Patient.find()
+            return res.status(200).json(patients)
+        } catch (e) {
+            return res.status(500).json({ message: e.message })
+        }
+    })
+    .post(async (req, res) => {
+        try {
+            let patient = new Patient(req.body)
+            patient = await patient.save()
+            return res.status(200).json(patient)
+        } catch (e) {
+            return res.status(500).json({ message: e.message })
+        }
+    })
+
+router
+    .route('/:patientID')
+    .patch(async (req, res) => {
+        try {
+            const { patientID } = req.params
+            const patient = await Patient.findOneAndUpdate({ id: patientID }, req.body, { new: true })
+            return res.status(200).json(patient)
+        } catch (e) {
+            return res.status(500).json({ message: e.message })
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            const { patientID } = req.params
+            const patient = await Patient.findOneAndDelete({ id: patientID })
+            return res.status(200).json(patient)
+        } catch (e) {
+            return res.status(500).json({ message: e.message })
+        }
+    })
 
 module.exports = router
