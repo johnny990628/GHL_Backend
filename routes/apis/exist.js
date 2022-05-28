c69345874888b945d03b7c6a95f1544d4b9baffc
johnny990628 <johnny99457@gmail.com>
2022-05-28 16:44:22 +0800

    feat: exists api

--- routes/apis/exist.js
+++ routes/apis/exist.js
@@ -3,11 +3,30 @@ const router = express.Router()
 
 const BLOOD = require('../../models/blood')
 const PATIENT = require('../../models/patient')
+const SCHEDULE = require('../../models/schedule')
 
-router.route('/').get(async (req, res) => {
+router.route('/patient').get(async (req, res) => {
     try {
-        const { id, blood } = req.query
-        const isExists = (id && (await PATIENT.exists({ id }))) || (blood && (await BLOOD.exists({ number: blood })))
+        const { value } = req.query
+        const isExists = await PATIENT.exists({ id: value })
+        return res.status(200).json(Boolean(isExists))
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
+    }
+})
+router.route('/blood').get(async (req, res) => {
+    try {
+        const { value } = req.query
+        const isExists = await BLOOD.exists({ number: value })
+        return res.status(200).json(Boolean(isExists))
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
+    }
+})
+router.route('/schedule').get(async (req, res) => {
+    try {
+        const { value } = req.query
+        const isExists = await SCHEDULE.exists({ patientID: value })
         return res.status(200).json(Boolean(isExists))
     } catch (e) {
         return res.status(500).json({ message: e.message })
