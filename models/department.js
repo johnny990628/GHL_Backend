2c460db43f9ec4053127a167ad25425bdba4a00f
johnny990628 <johnny99457@gmail.com>
2022-08-06 16:55:39 +0800

    refactor: add active col

--- models/department.js
+++ models/department.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const departmentSchema = new Schema({
     name: { type: String, required: true },
     address: { type: String, required: true },
+    active: { type: Boolean, required: true },
 })
 
 module.exports = mongoose.model('Department', departmentSchema)
