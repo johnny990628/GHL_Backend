7e3bc32404611143c76b14c46f6e7fc5ad3e5b12
johnny990628 <johnny99457@gmail.com>
2023-05-25 16:33:39 +0800

    Feat : dashboard api redesign

--- routes/apis/dashboard.js
+++ routes/apis/dashboard.js
@@ -6,79 +6,47 @@ const REPORT = require('../../models/report')
 const SCHEDULE = require('../../models/schedule')
 const PATIENT = require('../../models/patient')
 
-const statsData = async ({ matchConditions, conditions }) => {
-    const patients = await PATIENT.aggregate([...conditions])
-    const reports = await REPORT.aggregate([
-        ...conditions,
-        {
-            $project: {
-                records: 0,
-            },
-        },
-    ])
-    const schedules = await SCHEDULE.aggregate([...conditions])
+const statsData = async ({ departmentCondition, eventCondition }) => {
+    const patients = await PATIENT.aggregate([{ $match: departmentCondition }])
 
-    let patientCount = await PATIENT.aggregate([
-        {
-            $match: matchConditions,
-        },
+    const waitExaminationSchedule = await SCHEDULE.aggregate([{ $match: eventCondition }, { $match: { status: 'wait-examination' } }])
+    const finishSchedule = await SCHEDULE.aggregate([{ $match: eventCondition }, { $match: { status: 'finish' } }])
+
+    let patientCount = await PATIENT.aggregate([{ $match: departmentCondition }, { $group: { _id: null, n: { $sum: 1 } } }])
+
+    let waitExaminationCount = await SCHEDULE.aggregate([
+        { $match: eventCondition },
+        { $match: { status: 'wait-examination' } },
         { $group: { _id: null, n: { $sum: 1 } } },
     ])
-
-    let reportCount = await REPORT.aggregate([
-        {
-            $match: matchConditions,
-        },
-        { $group: { _id: null, n: { $sum: 1 } } },
-    ])
-    let scheduleCount = await SCHEDULE.aggregate([
-        {
-            $match: matchConditions,
-        },
+    let finishCount = await SCHEDULE.aggregate([
+        { $match: eventCondition },
+        { $match: { status: 'finish' } },
         { $group: { _id: null, n: { $sum: 1 } } },
     ])
 
     patientCount = patientCount.length ? patientCount[0]['n'] : 0
-    reportCount = reportCount.length ? reportCount[0]['n'] : 0
-    scheduleCount = scheduleCount.length ? scheduleCount[0]['n'] : 0
+    waitExaminationCount = waitExaminationCount.length ? waitExaminationCount[0]['n'] : 0
+    finishCount = finishCount.length ? finishCount[0]['n'] : 0
 
     return {
         patients,
-        reports,
-        schedules,
-        count: { patient: patientCount, report: reportCount, schedule: scheduleCount },
+        waitExaminationSchedule,
+        finishSchedule,
+        count: { patient: patientCount, waitExamination: waitExaminationCount, finish: finishCount },
     }
 }
 
 router.route('/').get(async (req, res) => {
     try {
-        const { dateFrom, dateTo, limit = 5 } = req.query
+        const { event, department } = req
 
-        const matchConditions = {
-            $and: [
-                {
-                    createdAt: {
-                        $gte: new Date(dateFrom),
-                        $lte: new Date(dateTo),
-                    },
-                },
-            ],
-        }
-        const conditions = [
-            {
-                $match: matchConditions,
-            },
-            {
-                $limit: limit * 1,
-            },
-        ]
-        const { patients, reports, schedules, count } = await statsData({ matchConditions, conditions })
-        return res.status(200).json({
-            patients,
-            reports,
-            schedules,
-            count,
-        })
+        const departmentCondition = department ? { departmentID: department } : {}
+
+        const eventCondition = event ? { eventID: event } : {}
+
+        const stats = await statsData({ departmentCondition, eventCondition })
+        return res.status(200).json(stats)
     } catch (e) {
         return res.status(500).json({ message: e.message })
     }
