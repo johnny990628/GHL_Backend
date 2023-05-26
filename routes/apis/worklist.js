3a3d60a638c08df09b0ec7377f2d79ca23fe202c
johnny99457 <johnny99457@gmail.com>
2023-05-26 18:25:58 +0800

    Refactor : modify worklist  api response

--- routes/apis/worklist.js
+++ routes/apis/worklist.js
@@ -1,26 +1,24 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-const router = express.Router();
-const PATIENT = require("../../models/patient");
-const { sendWorkItem } = require("../../dcm4chee/sendWorkItem");
-const {
-  POST_DCM4CHEE_workitems,
-  POST_DCM4CHEE_mwlitems,
-} = require("../../axios/DCM4CHEE");
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+const PATIENT = require('../../models/patient')
+const { sendWorkItem } = require('../../dcm4chee/sendWorkItem')
+const { POST_DCM4CHEE_workitems, POST_DCM4CHEE_mwlitems } = require('../../axios/DCM4CHEE')
 
-router.route("/:patientID").get(async (req, res) => {
-  try {
-    const { patientID } = req.params;
-    var patient = await PATIENT.findOne({ id: patientID });
-    var dicomTagData = await sendWorkItem(patient);
-    await POST_DCM4CHEE_workitems(dicomTagData);
-    const mwlitems = await POST_DCM4CHEE_mwlitems(dicomTagData);
-    
-    return res.status(200).json(mwlitems.data["0020000D"].Value);
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-});
+router.route('/:patientID').get(async (req, res) => {
+    try {
+        const { patientID } = req.params
+        var patient = await PATIENT.findOne({ id: patientID })
+        var dicomTagData = await sendWorkItem(patient)
+        await POST_DCM4CHEE_workitems(dicomTagData)
+        const mwlitems = await POST_DCM4CHEE_mwlitems(dicomTagData)
+        const studyInstanceUID = mwlitems.data['0020000D'].Value[0]
 
-module.exports = router;
+        return res.status(200).json({ studyInstanceUID })
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+})
+
+module.exports = router
